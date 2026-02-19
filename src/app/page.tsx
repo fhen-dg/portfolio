a11ae2ce9615341d23beb45fc881cf7cbaa76918
fhen-dg <federico.cohen.c@gmail.com>
2026-02-19 12:36:23 -0300

    Add UI Kit styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,11 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between text-sm">
-        <h1 className="text-4xl font-bold text-center mb-4">
+        <h1 className="text-center mb-4 header1 text-primary-base">
           Welcome to My Portfolio
         </h1>
         <Button>Click me</Button>
-        <p className="text-center text-lg text-gray-600">
+        <p className="text-center text-lg text-neutral-400">
           Built with Next.js, TypeScript, and Tailwind CSS
         </p>
       </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 const config: Config = {
   darkMode: ["class"],
@@ -23,9 +24,24 @@ const config: Config = {
           DEFAULT: "hsl(var(--popover))",
           foreground: "hsl(var(--popover-foreground))",
         },
+        neutral: {
+          50: "#FDFDFD",
+          100: "#F8F8F8",
+          200: "#F3F3F3",
+          300: "#EEEEEE",
+          400: "#DBDBDB",
+          500: "#A8A8A8",
+          600: "#7B7B7B",
+          700: "#5C5C5C",
+          800: "#383838",
+          900: "#222222",
+          white: "#FFFFFF",
+          black: "#000000",
+        },
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
+          base: "#E50041",
         },
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
@@ -54,7 +70,87 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        ".header1": {
+          fontSize: "64px",
+          lineHeight: "150%",
+          fontWeight: "700",
+        },
+        ".header3": {
+          fontSize: "44px",
+          lineHeight: "150%",
+          fontWeight: "700",
+        },
+        ".title1": {
+          fontSize: "40px",
+          lineHeight: "150%",
+          fontWeight: "700",
+        },
+        ".title2": {
+          fontSize: "32px",
+          lineHeight: "175%",
+          fontWeight: "700",
+        },
+        ".title3": {
+          fontSize: "28px",
+          lineHeight: "175%",
+          fontWeight: "700",
+        },
+        ".title4": {
+          fontSize: "24px",
+          lineHeight: "150%",
+          fontWeight: "700",
+        },
+        ".title5": {
+          fontSize: "20px",
+          lineHeight: "150%",
+          fontWeight: "700",
+        },
+        ".supporting1": {
+          fontSize: "32px",
+          lineHeight: "175%",
+        },
+        ".supporting1-medium": {
+          fontSize: "32px",
+          lineHeight: "175%",
+          fontWeight: "500",
+        },
+        ".supporting2": {
+          fontSize: "24px",
+          lineHeight: "175%",
+        },
+        ".supporting2-medium": {
+          fontSize: "24px",
+          lineHeight: "175%",
+          fontWeight: "500",
+        },
+        ".body1": {
+          fontSize: "18px",
+          lineHeight: "175%",
+        },
+        ".body1-bold": {
+          fontSize: "18px",
+          lineHeight: "175%",
+          fontWeight: "700",
+        },
+        ".body2": {
+          fontSize: "16px",
+          lineHeight: "175%",
+        },
+        ".body3": {
+          fontSize: "14px",
+          lineHeight: "175%",
+        },
+        ".body3-bold": {
+          fontSize: "14px",
+          lineHeight: "175%",
+          fontWeight: "700",
+        },
+      });
+    }),
+  ],
 };
 
 export default config;
