b90857b48b5b38104f2376ec4db80656586c50cd
fhen-dg <federico.cohen.c@gmail.com>
2026-02-19 15:26:40 -0300

    Add DropdownMenu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { IconButton } from "@/components/ui/iconButton"
-import { SendHorizonal } from "lucide-react"
+import { Languages } from "lucide-react"
 import { useLocale } from "@/hooks/useLocale";
+import type { Locale } from "@/lib/i18n/types";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function Home() {
   const { t, locale, setLocale } = useLocale();
@@ -12,18 +19,23 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between text-sm">
         <div className="flex justify-end gap-2 mb-8">
-          <Button
-            onClick={() => setLocale(locale === "en" ? "es" : "en")}
-          >
-            {locale === "en" ? "Español" : "English"}
-          </Button>
-          <Button variant="ghost">Chupete</Button>
-          <IconButton>
-  <SendHorizonal />
-</IconButton>
-<IconButton variant="ghost">
-  <SendHorizonal />
-</IconButton>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <IconButton variant="ghost">
+                <Languages />
+              </IconButton>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuRadioGroup value={locale} onValueChange={(value) => setLocale(value as Locale)}>
+                <DropdownMenuRadioItem value="en">
+                  English
+                </DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="es">
+                  Español
+                </DropdownMenuRadioItem>
+              </DropdownMenuRadioGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
         
         <h1 className="text-center mb-4 header1 text-neutral-800">
